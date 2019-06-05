5c3fe9d315ae07864ce9ed693e1e326408b37d8d
Shreyas Jayanna <shreyasj2006@gmail.com>
2019-06-05 10:56:00 -0700

    feat(retrieve_movies): Setup GET endpoint to retrieve movies (#8)
    * feat(retrieve_movies): Setup GET endpoint to retrieve movies
    
    * Address CR comments

--- test/plugins/features/movies/controller.test.js
+++ test/plugins/features/movies/controller.test.js
@@ -2,6 +2,24 @@
 
 const Controller = require('../../../../lib/plugins/features/movies/controller');
 
+const Knex = require('../../../../lib/libraries/knex');
+
+const TableName = 'movies';
+
+beforeEach('Setup test DB', async () => {
+  await Knex(TableName).truncate();
+  await Knex(TableName).insert([
+    {
+      title: 'Twisted',
+      release_year: 2004
+    },
+    {
+      title: 'Never Die Twice',
+      release_year: 2001
+    }
+  ]);
+});
+
 describe('movie controller', () => {
 
   describe('create', () => {
@@ -12,6 +30,41 @@ describe('movie controller', () => {
       const movie = await Controller.create(payload);
 
       expect(movie.get('title')).to.eql(payload.title);
+    });
+
+  });
+
+  describe('retrieve', () => {
+
+    it('retrieves all movies', async () => {
+      const movies = await Controller.retrieve();
+
+      expect(movies.length).to.eql(2);
+    });
+
+    it('retrieves movies by title', async () => {
+      const queryParams = { title: 'Twi' };
+      const movies = await Controller.retrieve(queryParams);
+
+      expect(movies.length).to.eql(2);
+    });
+
+    it('retrieves movies by release year', async () => {
+      const queryParams = { year: 2004 };
+      const movies = await Controller.retrieve(queryParams);
+
+      expect(movies.length).to.eql(1);
+      expect(movies.at(0).get('release_year')).to.eql(2004);
+    });
+
+    it('retrieves movies by range of release year', async () => {
+      const queryParams = {
+        year_from: 2000,
+        year_to: 2004
+      };
+      const movies = await Controller.retrieve(queryParams);
+
+      expect(movies.length).to.eql(2);
     });
 
   });
--- test/plugins/features/movies/index.test.js
+++ test/plugins/features/movies/index.test.js
@@ -20,4 +20,72 @@ describe('movies integration', () => {
 
   });
 
+  describe('retrieve movies', () => {
+
+    it('retrieve all movies', () => {
+      return Movies.inject({
+        url: '/movies',
+        method: 'GET'
+      })
+      .then((response) => {
+        expect(response.statusCode).to.eql(200);
+        expect(response.result).to.be.an('array');
+      });
+    });
+
+    it('retrieve movies by name', () => {
+      const title = 'Twi';
+
+      return Movies.inject({
+        url: `/movies?title=${title}`,
+        method: 'GET'
+      })
+      .then((response) => {
+        expect(response.statusCode).to.eql(200);
+        expect(response.result).to.be.an('array');
+
+        for (const idx in response.result) {
+          expect(response.result[idx].title).to.include(title);
+        }
+      });
+    });
+
+    it('retrieve movies by year', () => {
+      const year = 2000;
+
+      return Movies.inject({
+        url: `/movies?year=${year}`,
+        method: 'GET'
+      })
+      .then((response) => {
+        expect(response.statusCode).to.eql(200);
+        expect(response.result).to.be.an('array');
+
+        for (const idx in response.result) {
+          expect(response.result[idx].release_year).to.eql(year);
+        }
+      });
+    });
+
+    it('retrieve movies within a range of years', () => {
+      const year_from = 2000;
+      const year_to = 2002;
+
+      return Movies.inject({
+        url: `/movies?year_from=${year_from}&year_to=${year_to}`,
+        method: 'GET'
+      })
+      .then((response) => {
+        expect(response.statusCode).to.eql(200);
+        expect(response.result).to.be.an('array');
+
+        for (const idx in response.result) {
+          expect(response.result[idx].release_year).to.greaterThan(year_from - 1);
+          expect(response.result[idx].release_year).to.lessThan(year_to + 1);
+        }
+      });
+    });
+
+  });
+
 });
